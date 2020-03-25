6db1c0f37a9ef7a6edb9aa1a9b0a978c4fb31c6e
vangeest <gee@emmauscollege.nl>
2020-03-25 20:36:46 +0000

    Voorbeeld functies toegevoegd

--- script.js
+++ script.js
@@ -1,49 +1,80 @@
 /* Game opdracht
-   Versie 20190113GEE, voor Emmauscollege Rotterdam
+   Informatica - Emmauscollege Rotterdam
    Template voor een game in JavaScript met de p5 library
    Begin met dit template voor je game opdracht,
    voeg er je eigen code aan toe.
-*/
+ */
 
 /* globale variabelen die je gebruikt in je game
-   */
+ */
+
+const UITLEG = 0;
+const SPELEN = 1;
+const GAMEOVER = 2;
+var spelStatus = SPELEN;
+
+var spelerX = 200; // x-positie van speler
+var spelerY = 100; // y-positie van speler
 
 /* functies die je gebruikt in je game
-   */
+ */
+
+var tekenVeld = function () {
+  // teken het veld
+  fill("green");
+  rect(20, 20, width - 2 * 20, height - 2 * 20);
+}
+
+var tekenVijand = function() {
+  // teken de vijand
+};
+
+var tekenSpeler = function(x, y) {
+  // teken de speler
+  fill("white");
+  ellipse(x, y, 50, 50);
+};
+
+var verplaatsVijand = function() {
+  // update globale variabelen met positie van vijand
+};
+
+var verplaatsSpeler = function() {
+  // kijk wat de toetsen/muis etc zijn
+  // update globale variabele spelerX en spelerY aan
+};
+
+var checkBotsing = function() {
+  // zoek uit of er een botsing is
+  return false;
+};
 
 /* setup
    de code in deze functie wordt één keer uitgevoerd door de p5 library, 
    zodra het spel geladen is in de browser
    */
-function setup() { 
-  createCanvas (1280, 720); // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
-  background ('blue'); // Kleur de achtergrond blauw, zodat je het kunt zien
+function setup() {
+  createCanvas(1280, 720); // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+  background('blue'); // Kleur de achtergrond blauw, zodat je het kunt zien
 }
 
 /* draw
    de code in deze functie wordt meerdere keren per seconde uitgevoerd door de p5 library, 
    nadat de setup functie klaar is
    */
-function draw() { 
-
-  /* teken het speelveld, gebruik de globale variabelen
-   */
-  fill("green");
-  for (var x = 0; x < width/2; x = x + width/20){
-
-  rect(x+10, x * height/width+10,
-       width - 2 * x-20, height - 2 * x * height/width-20);
-  }
-  /* verwerk een stap in de tijd: 
-     pas de waarden van de globale variabelen aan
-   */
-
-  /* verwerk invoer van toetsenbord / muis / touchscreen:
-     pas de waarden van de globale variabelen aan
-    */
+function draw() {
+  switch (spelStatus) {
+    case SPELEN:
+      verplaatsVijand();
+      verplaatsSpeler();
   
-  /* controlleer of het spel klaar is
-     als het klaar is, wacht dan tot de speler opnieuw begint
-   */
-}
+      tekenVeld();
+      tekenVijand();
+      tekenSpeler(spelerX, spelerY);
 
+      if (checkBotsing()) {
+        SPELSTATUS = GAMEOVER;
+      }
+      break;
+  }
+}
