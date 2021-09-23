d4744483f8bd91dd2dffe53383c00c8b865c3f99
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2021-09-23 22:52:08 +0000

    add planning and simplified code

--- README.md
+++ README.md
@@ -7,24 +7,40 @@ Gemaakt door:
 
 ## Mijn planning
 
-Basis: uitleg, spelen, afgaan en punten
+Basis: uitleg, spelen, afgaan en punten\
+(onderdelen tussen haakjes kun je overslaan)
 - [x] maak index.html, style.css en script.js met canvas
 - [ ] teken speler
-- [ ] beweeg speler
-- [ ] ... *(vul zelf aan)*
-- [ ] punten op scherm zetten
-- [ ] punten kunnen scoren
-- [ ] uitlegscherm
+- [ ] beweeg speler met pijltjetoetsen
+- [ ] beperk beweging tot schermranden
+- [ ] tekenVijand
+- [ ] beweegVijand vallend
+- [ ] zet vijand bovenaan als gevallen
+- [ ] console-bericht bij botsing speler-vijand
+- [ ] toon HP, elke botsing is lagere HP
+- [ ] toon punten, elke seconde 1 punt erbij
+- [ ] game over scherm met punten als HP op is
+- [ ] (intro scherm met speluitleg, enter is start)
+- [ ] maak 3 vijanden op een rij
+- [ ] maak 10 vijanden op een rij met een loop
+- [ ] (maak 10 vijanden op willekeurige plekken met een loop en array)
+- [ ] tekenKogel ergens
+- [ ] zet kogel bij speler als spatie wordt ingedrukt
+- [ ] beweegKogel omhoog
+- [ ] console-bericht bij botsing kogel-vijand
+- [ ] verwijder kogel en vijand als geraakt
+- [ ] (maak 10 kogels met loop en array)
+- [ ] (schiet maar 1 kogel telkens als spatie wordt ingedrukt)
 
-Uitbreiding: *bedenk iets, bijvoorbeeld meer tegenstanders*
-- [ ] *later uitwerken*
-- [ ] *later uitwerken*
-- [ ] *later uitwerken*
-
-Uitbreiding: *bedenk iets, bijvoorbeeld bewegende achtergrond*
-- [ ] *later uitwerken*
-- [ ] *later uitwerken*
-- [ ] *later uitwerken*
+Uitbreiding:\
+Kies je eigen volgorde
+- [ ] Gebruik plaatjes voor de vijanden
+- [ ] Animeer speler
+- [ ] Laat badges vallen die extra's aan speler geven
+- [ ] Maak spel steeds moeilijker door toenemend aantal vijanden met tekenVijand
+- [ ] Maak andere vijanden bij toenemen tekenVijand
+- [ ] Laat vijanden bewegen in patronen
+- [ ] ...
 
 ## Credits
 - Game template van het Emmauscollege Rotterdam https://github.com/emmauscollege/4HV-game-template
--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/// @ts-check
-/// <reference path=".gitpod/p5.global-mode.d.ts" />
-"use strict";
-
 /* Game opdracht
    Informatica - Emmauscollege Rotterdam
    Template voor een game in JavaScript met de p5 library
@@ -9,9 +5,6 @@
    Begin met dit template voor je game opdracht,
    voeg er je eigen code aan toe.
  */
-
-
-
 
 /* ********************************************* */
 /* globale variabelen die je gebruikt in je game */
@@ -25,120 +18,33 @@ var spelStatus = SPELEN;
 var spelerX = 200; // x-positie van speler
 var spelerY = 100; // y-positie van speler
 
-var kogelX = 0;    // x-positie van kogel
-var kogelY = 0;    // y-positie van kogel
-
-var vijandX = 0;   // x-positie van vijand
-var vijandY = 0;   // y-positie van vijand
-
-var score = 0; // aantal behaalde punten
-
-
-
-
-
 /* ********************************************* */
 /*      functies die je gebruikt in je game      */
 /* ********************************************* */
 
-
 /**
- * Tekent het speelveld
+ * Tekent spelscherm
  */
-var tekenVeld = function () {
-  fill("purple");
-  rect(20, 20, width - 2 * 20, height - 2 * 20);
-};
+var tekenAlles = function() {
+  // achtergrond
 
+  // vijand
 
-/**
- * Tekent de vijand
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenVijand = function(x, y) {
-    
+  // kogel
 
-};
-
-
-/**
- * Tekent de kogel of de bal
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenKogel = function(x, y) {
-
-
-};
-
-
-/**
- * Tekent de speler
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenSpeler = function(x, y) {
+  // speler
   fill("white");
   ellipse(x, y, 50, 50);
+
+  // punten en HP
 };
 
-
 /**
- * Updatet globale variabelen met positie van vijand of tegenspeler
+ * Updatet globale variabelen met posities van speler, vijanden en kogels
  */
-var beweegVijand = function() {
+var beweegAlles = function() {
     
 };
-
-
-/**
- * Updatet globale variabelen met positie van kogel of bal
- */
-var beweegKogel = function() {
-
-};
-
-
-/**
- * Kijkt wat de toetsen/muis etc zijn.
- * Updatet globale variabele spelerX en spelerY
- */
-var beweegSpeler = function() {
-
-};
-
-
-/**
- * Zoekt uit of de vijand is geraakt
- * @returns {boolean} true als vijand is geraakt
- */
-var checkVijandGeraakt = function() {
-
-  return false;
-};
-
-
-/**
- * Zoekt uit of de speler is geraakt
- * bijvoorbeeld door botsing met vijand
- * @returns {boolean} true als speler is geraakt
- */
-var checkSpelerGeraakt = function() {
-    
-  return false;
-};
-
-
-/**
- * Zoekt uit of het spel is afgelopen
- * @returns {boolean} true als het spel is afgelopen
- */
-var checkGameOver = function() {
-    
-  return false;
-};
-
 
 /**
  * setup
@@ -153,7 +59,6 @@ function setup() {
   background('blue');
 }
 
-
 /**
  * draw
  * de code in deze functie wordt meerdere keren per seconde
@@ -162,25 +67,10 @@ function setup() {
 function draw() {
   switch (spelStatus) {
     case SPELEN:
-      beweegVijand();
-      beweegKogel();
-      beweegSpeler();
-      
-      if (checkVijandGeraakt()) {
-        // punten erbij
-        // nieuwe vijand maken
-      }
-      
-      if (checkSpelerGeraakt()) {
-        // leven eraf of gezondheid verlagen
-        // eventueel: nieuwe speler maken
-      }
-
-      tekenVeld();
-      tekenVijand(vijandX, vijandY);
-      tekenKogel(kogelX, kogelY);
-      tekenSpeler(spelerX, spelerY);
-
+      beweegAlles();
+      updatePunten();
+      updateHealth();
+      tekenAlles();
       if (checkGameOver()) {
         spelStatus = GAMEOVER;
       }
